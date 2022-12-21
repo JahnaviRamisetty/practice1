bba30b12fe30214ffc8904986c41741b5399b21a
jahnavi <chinnaramisetty99@gmail.com>
2022-12-21 16:49:54 +0530

    first commit

---
+++ pageobjectmodel/row_table.py
@@ -0,0 +1,17 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from selenium import webdriver
+from selenium .webdriver .support.BY import
+from selenium import webdriver
+
+
+class Executable_Path:
+    pass
+
+
+driver=webdriver.Chrome(Executable_Path"")
+table_class="mat-sort table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+
+table_header="/html/body/app-root/app-simpletable/section[1]/div/div/div[1]/div/div/div/div[3]/div/table/thead"
+row= driver.find_elements(BY.XPATH("/html/body/table/tbody/tr[]"))
+col=driver.find_elements(BY.XPATH("/html/body/table/tbody/tr[1],td[]"))
